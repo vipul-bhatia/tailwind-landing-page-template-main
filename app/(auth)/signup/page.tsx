1cbc3979324449913094307742d7d494056b65ee
vipul-bhatia <vipul0592bhatia@gmail.com>
2023-09-26 13:33:06 +0530

    .

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 export const metadata = {
   title: 'Contact Us',
   description: 'Page description',
 }
-"use client";
+
 
 import React, { useState } from 'react';
 import { db } from '../../../firebase/config.js';
@@ -41,8 +43,8 @@ export default function SignUp() {
           </div>
 
           {/* Form */}
-          <div className="max-w-sm mx-auto" onSubmit={handleSubscribe}>
-            <form>
+          <div className="max-w-sm mx-auto" >
+            <form onSubmit={handleSubscribe}>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
                   <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Name <span className="text-red-600">*</span></label>
