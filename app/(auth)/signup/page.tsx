645cb46eba3480955c4de9d4568554a86bd24e64
vipul-bhatia <vipul0592bhatia@gmail.com>
2023-09-28 00:47:41 +0530

    .

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -55,9 +55,10 @@ export default function SignUp() {
               </div>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="Phone No." >Phone No.<span className="text-red-600">*</span></label>
-                  <input id="password" type="number" className="form-input w-full text-gray-800" placeholder="Enter your number" required value={contact}
-          onChange={e => setContact(e.target.value)}/>
+                <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="contact">Phone No.<span className="text-red-600">*</span></label>
+<input id="contact" type="number" className="form-input w-full text-gray-800" placeholder="Enter your number" required value={contact}
+       onChange={e => setContact(e.target.value)}/>
+
                 </div>
               </div>
               <div className="flex flex-wrap -mx-3 mt-6">
@@ -70,7 +71,8 @@ export default function SignUp() {
               </div>
             </form>
            
-           
+            {message && <div className="text-center mt-4">{message}</div>}
+
             
           </div>
 
